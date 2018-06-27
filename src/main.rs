66dfc4461f923eadd548d830dd50387bcfb42ed0
Brandon Bauley <bbauley@pdx.edu>
2018-06-27 16:37:08 -0700

    Now tests to make sure that arguments have valid length

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,19 @@
+fn invalid_length_exception() {
+	println!("usage target/debug/calc <operation> <num1> <num2> ... <numN>");
+	println!("example: target/debug/calc sum 4 6 10 12");
+	std::process::exit(0);
+}
+
 fn main() {
-    println!("Hello, world!");
-	//test
+
+	let length = std::env::args().len();
+	if length < 3
+	{
+		invalid_length_exception();
+	}
+	for arg in std::env::args()
+	{
+    	println!("{}", arg);	
+	}
+	
 }
