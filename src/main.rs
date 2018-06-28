e3c340a73e091dded37c2a69639cf7b5c8adb0c7
Brandon Bauley <bbauley@pdx.edu>
2018-06-27 17:35:50 -0700

    Can now build vector off of values from command line with error checking.

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,33 @@
+use std::env;
+use std::process;
+use std::str::FromStr;
+
 fn invalid_length_exception() {
-	println!("usage target/debug/calc <operation> <num1> <num2> ... <numN>");
+	println!("usage: target/debug/calc <operation> <num1> <num2> ... <numN>");
 	println!("example: target/debug/calc sum 4 6 10 12");
-	std::process::exit(0);
+	process::exit(0);
 }
 
 fn main() {
 
-	let length = std::env::args().len();
+	let mut numbers = Vec::new();
+	let length = env::args().len();
+	let operation = env::args().nth(1);
+
 	if length < 3
 	{
 		invalid_length_exception();
 	}
-	for arg in std::env::args()
+	for arg in env::args().skip(2)	//Need to ignore first two arguments
 	{
-    	println!("{}", arg);	
+    	numbers.push(u64::from_str(&arg)
+			 .expect("error parsing argument"));	
 	}
+	if numbers.len() == 1
+	{
+		println!("{}", numbers[0]);
+		process::exit(0);
+	}
+	
 	
 }
